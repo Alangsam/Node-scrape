d51b063f57f2b9ee30a35b8294caa8e5c2ac365b
Alangsam <alex@Alexs-MacBook-Pro.local>
2020-08-05 22:14:29 -0700

    all data retrieved and formatted

--- scrape.js
+++ scrape.js
@@ -2,15 +2,35 @@ const fs = require("fs");
 const request = require("request");
 const { getComponents, getValues } = require("./regex");
 
-// request(
-//     "https://spreadsheets.google.com/feeds/list/1KGPDThGon6pDzDoaxrBAW_Dru4ZomAZe_FwPWgF7QwQ/od6/public/values?alt=json",
-//     (error, response, body) => {
-//         //const data = JSON.parse(body);
-//         console.log("body:", body[1]);
+let theBody;
+request(
+    "https://spreadsheets.google.com/feeds/list/1KGPDThGon6pDzDoaxrBAW_Dru4ZomAZe_FwPWgF7QwQ/od6/public/values?alt=json",
+    (error, response, body) => {
+        const newArr = getComponents(body);
+        const finalArr = getValues(String(newArr));
+        let theArr = [];
+        do {
+            theArr.push(finalArr.splice(0, 5));
+        } while (finalArr.length > 0);
+        const firstCompany = theArr.splice(0, 12507);
+        const secondCompany = theArr.splice(1, 12507);
+        const thirdCompany = theArr.splice(2, 12507);
+        const fourthCompany = theArr.splice(3, 12507);
+        console.log(firstCompany);
+        console.log(secondCompany);
+        console.log(thirdCompany);
+        console.log(fourthCompany);
+        fs.writeFileSync("companies/one.json", JSON.stringify(firstCompany));
+        fs.writeFileSync("companies/two.json", JSON.stringify(secondCompany));
+        fs.writeFileSync("companies/three.json", JSON.stringify(thirdCompany));
+        fs.writeFileSync("companies/four.json", JSON.stringify(fourthCompany));
+        // theBody = JSON.parse(body);
+        // console.log(finalArr);
+        //console.log("body:", body);
 
-//         fs.writeFileSync("companies/one.json", data);
-//     }
-// );
+        //fs.writeFileSync("companies/one.json", data);
+    }
+);
 
 // const basicFile = String(fs.readFileSync("companies/one.json"));
 // const newArr = getComponents(basicFile);
@@ -20,12 +40,15 @@ const { getComponents, getValues } = require("./regex");
 // const newArrs = String(fs.readFileSync("one.json"));
 // console.log(getValues(newArrs));
 // fs.writeFileSync("letsSee.js", JSON.stringify(getValues(newArrs)));
-const newArrs = String(fs.readFileSync("letsSee.js"));
-console.log(JSON.parse(newArrs)[0]);
-let copyTheArr = JSON.parse(newArrs);
-let theArr = [];
-do {
-    theArr.push(copyTheArr.splice(0, 5));
-} while (copyTheArr.length > 0);
-console.log(theArr);
-fs.writeFileSync("./companies/one.js", JSON.stringify(theArr));
+
+// const newArrs = String(fs.readFileSync("letsSee.js"));
+// console.log(JSON.parse(newArrs)[0]);
+// let copyTheArr = JSON.parse(newArrs);
+// let theArr = [];
+// do {
+//     theArr.push(copyTheArr.splice(0, 5));
+// } while (copyTheArr.length > 0);
+// console.log(theArr);
+// fs.writeFileSync("./companies/one.js", JSON.stringify(theArr));
+
+//12507
